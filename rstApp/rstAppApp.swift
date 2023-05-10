10357db7f597455c0079bbc05016503363ac52b7
gregorian.st <gregorian.st@gmail.com>
2023-05-10 19:23:10 +0200

    Initial Commit

---
+++ rstApp/rstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  rstAppApp.swift
+//  rstApp
+//
+//  Created by Grigory Stolyarov on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct rstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
